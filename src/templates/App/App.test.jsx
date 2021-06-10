d0019185751980dd3b5397d4760c4ef6376e20ec
Jos3s <karatedkgt17@gmail.com>
2021-06-09 21:56:03 -0300

    Configurando testes com jest-styled-components

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { screen } from '@testing-library/react';
+import Home from '.';
+import { renderTheme } from '../../styles/renderTheme';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  renderTheme(<Home />);
+  const headingContainer = screen.getByRole('heading', { name: 'Hello World' }).parentElement;
+  expect(headingContainer).toHaveStyle({
+    background: 'tomato',
+  });
+  expect(headingContainer).toMatchSnapshot();
+  expect(headingContainer).toHaveStyleRule('background', 'tomato');
 });
--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -3,9 +3,6 @@ import * as Styled from './styles';
 function Home() {
   return (
     <div>
-      <Styled.Wrapper background="blue">
-        <h1>Hello World</h1>
-      </Styled.Wrapper>
       <Styled.Wrapper background="tomato" color="white">
         <h1>Hello World</h1>
       </Styled.Wrapper>
