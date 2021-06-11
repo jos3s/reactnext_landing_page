8cf67d590a0ed8aadff0a82597cd914d08d42b97
Jos3s <karatedkgt17@gmail.com>
2021-06-11 19:38:41 -0300

    Definindo fonts e outra informações do tema global

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,36 @@
 export const theme = {
   colors: {
-    mainBg: 'yellow',
-    secondaryBg: 'blue',
+    primaryColor: '#0A1128',
+    secondaryColor: '#dc143c',
+    white: '#FFFFFF',
+    mediumGray: '#DDDDDD',
   },
-  textColors: {
-    mainText: 'black',
+  font: {
+    family: {
+      default: "'Open Sans', sans-serif",
+      titles: "'Montserrat', sans-serif",
+    },
+    sizes: {
+      xsmall: '8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+    },
+    media: {
+      lteMedium: '(max-width: 768px)',
+    },
+    spacings: {
+      xsmall: '8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+      xhuge: '6.4rem',
+    },
   },
 };
