70c91aa772110e973d8ba92ddd134b07aa65e986
Jos3s <karatedkgt17@gmail.com>
2021-06-13 23:23:47 -0300

    Corrigindo estilos no SectionContainer

--- src/components/SectionContainer/styles.js
+++ src/components/SectionContainer/styles.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 1200px;
+  max-width: 120rem;
   margin: 0 auto;
   padding: ${({ theme }) => theme.spacings.large};
+  width: 100%;
 `;
