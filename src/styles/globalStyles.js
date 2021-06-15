34852dc5405469bfd0bc782fcf1bc2e09460ff11
Jos3s <karatedkgt17@gmail.com>
2021-06-14 21:49:26 -0300

    Adicionado estilos globais de outros elementos

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&display=swap');
 
   *{
     margin:0;
@@ -15,6 +16,17 @@ export const GlobalStyles = createGlobalStyle`
     font-family: ${({ theme }) => theme.font.family.default};
   }
   h1,h2,h3,h4,h5,h6{
-    font-family: ${({ theme }) => theme.font.family.titles};
+    font-family: 'Montserrat', sans-serif;
+    margin:${({ theme }) => theme.spacings.large} 0;
+  }
+  p{
+    margin:${({ theme }) => theme.spacings.medium} 0;
+  }
+  ul,ol{
+    margin:${({ theme }) => theme.spacings.medium} 0;
+    padding:${({ theme }) => theme.spacings.medium} 0;
+  }
+  a{
+    color:${({ theme }) => theme.colors.secondaryColor} 0;
   }
 `;
