17e5342d27e88e29e4447ba568ce711ffcf68ef6
Jos3s <karatedkgt17@gmail.com>
2021-06-15 19:42:44 -0300

    Corrigindo problemas com os componentes

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,10 +1,22 @@
 import styled from 'styled-components';
+import { Container as TextComponent } from '../../components/TextComponent/styles';
+import { Container as SectionContainer } from '../../components/SectionContainer/styles';
 
 export const Container = styled.footer`
   text-align: center;
+  border-top: 0.1rem solid ${({ theme }) => theme.colors.mediumGray};
+
   a {
     color: inherit;
     text-decoration: none;
+  }
+
+  & ${TextComponent} {
     font-size: ${({ theme }) => theme.font.sizes.small};
+  }
+
+  & ${SectionContainer} {
+    padding-top: 0;
+    padding-bottom: 0;
   }
 `;
