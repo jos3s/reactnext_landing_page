5c6b3c77d2648676c6a52ab040c1b0ec1f4d0af0
Jos3s <karatedkgt17@gmail.com>
2021-06-15 22:44:42 -0300

    Testando e mapeando dados da SectionTwoColumn

---
+++ src/api/mapSections.js
@@ -0,0 +1,45 @@
+export const mapSections = (sections = []) => {
+  return sections.map((section) => {
+    if (section.__component === 'section.section-two-columns') {
+      return mapSectionTwoColumn(section);
+    }
+    if (section.__component === 'section.section-content') {
+      return mapSectionContent(section);
+    }
+    if (section.__component === 'section.section-grid') {
+      return mapSectionGrid(section);
+    }
+
+    return section;
+  });
+};
+
+export const mapSectionTwoColumn = (section = {}) => {
+  const {
+    __component: component = '',
+    title = '',
+    description: text = '',
+    image: { url: srcImg = '' } = '',
+    metadata: {
+      background: backgroundDark = false,
+      section_id: sectionId = '',
+    } = false,
+  } = section;
+
+  return {
+    component,
+    title,
+    text,
+    srcImg,
+    backgroundDark,
+    sectionId,
+  };
+};
+
+export const mapSectionContent = (section) => {
+  return section;
+};
+
+export const mapSectionGrid = (section) => {
+  return section;
+};
---
+++ src/api/mapSections.test.js
@@ -0,0 +1,69 @@
+import { mapSections, mapSectionTwoColumn } from './mapSections';
+
+describe('mapSections', () => {
+  it('should render predefined sections if no data', () => {
+    const data = mapSections();
+
+    expect(data).toEqual([]);
+  });
+  it('should map section two column', () => {
+    const data = mapSectionTwoColumn();
+
+    expect(data.backgroundDark).toBe(false);
+    expect(data.component).toBe('');
+    expect(data.sectionId).toBe('');
+    expect(data.srcImg).toBe('');
+    expect(data.text).toBe('');
+    expect(data.title).toBe('');
+  });
+
+  it('should map section two column', () => {
+    const data = mapSectionTwoColumn({
+      __component: 'section.section-two-columns',
+      _id: '60c13d913c48bd3f64fc0e5a',
+      description: 'To wrap up January',
+      title: 'January brings us Firefox 85',
+      metadata: {
+        background: true,
+        _id: '60c13d923c48bd3f64fc0e61',
+        name: 'Home',
+        section_id: 'home',
+        __v: 0,
+        id: '60c13d923c48bd3f64fc0e61',
+      },
+      __v: 1,
+      image: {
+        _id: '60c1029a3c48bd3f64fc0e57',
+        name: 'javascript.svg',
+        alternativeText:
+          'Desenho de pessoas segurando os logos do HTML, CSS E JS',
+        caption: 'Desenho de pessoas segurando os logos do HTML, CSS E JS',
+        hash: 'javascript_2ac7d6a0f8',
+        ext: '.svg',
+        mime: 'image/svg+xml',
+        size: 30.31,
+        url: '2ac7d6a0f8.svg',
+        provider_metadata: {
+          public_id: 'javascript_2ac7d6a0f8',
+          resource_type: 'image',
+        },
+        provider: 'cloudinary',
+        width: 1030,
+        height: 730,
+        related: ['60c13d913c48bd3f64fc0e5a', '60c13d913c48bd3f64fc0e60'],
+        createdAt: '2021-06-09T18:04:10.082Z',
+        updatedAt: '2021-06-09T22:15:47.752Z',
+        __v: 0,
+        id: '60c1029a3c48bd3f64fc0e57',
+      },
+      id: '60c13d913c48bd3f64fc0e5a',
+    });
+
+    expect(data.backgroundDark).toBe(true);
+    expect(data.component).toBe('section.section-two-columns');
+    expect(data.sectionId).toBe('home');
+    expect(data.srcImg).toBe('2ac7d6a0f8.svg');
+    expect(data.text).toBe('To wrap up January');
+    expect(data.title).toBe('January brings us Firefox 85');
+  });
+});
