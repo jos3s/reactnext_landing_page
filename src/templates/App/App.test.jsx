e0b83e45805d4dac03ad8769617cffd35fdcad10
Jos3s <karatedkgt17@gmail.com>
2021-06-19 21:15:37 -0300

    Removendo testes dos templates

--- src/templates/App/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import Home from '.';
-import { renderTheme } from '../../styles/renderTheme';
-
-describe('<Home />', () => {
-  it('should render home', () => {
-    renderTheme(<Home />);
-  });
-});
