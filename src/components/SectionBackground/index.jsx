dce2b9c4c4f90408b441be105b82aa4a3116057c
Jos3s <karatedkgt17@gmail.com>
2021-07-02 00:46:52 -0300

    Corrigindo erro de ID

--- src/components/SectionBackground/index.jsx
+++ src/components/SectionBackground/index.jsx
@@ -2,17 +2,13 @@ import P from 'prop-types';
 import { SectionContainer } from '../SectionContainer';
 import * as Styled from './styles';
 
-const random = () => `id-${Math.random() * 1000}`.replace(/[^a-z0-9-_]/gi, '-');
-
 export const SectionBackground = ({
   children,
   backgroundDark = false,
   sectionId = 'sectionId',
 }) => {
-  const id = sectionId ? sectionId : random;
-
   return (
-    <Styled.Container backgroundDark={backgroundDark} id={id}>
+    <Styled.Container backgroundDark={backgroundDark} id={sectionId}>
       <SectionContainer>{children}</SectionContainer>
     </Styled.Container>
   );
